cdc233cc4ab5cd920f9c9f572f014c059efa47c5
Ivan Hawkes <ivan.hawkes@gmail.com>
2026-03-26 11:39:32 +1000

    Added strategy pools and a round robin function to use them evenly.

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"log"
 	"slices"
+	"sync"
 	"time"
 
 	"ivanhawkes.dev/snowflake"
 )
 
 type reservePoolType []uint32
+type strategyPoolListType []*Strategy
+
+type StrategyPool struct {
+	Pool      strategyPoolListType
+	length    uint32
+	poolIndex uint32
+}
 
 type Strategy struct {
 	snowflake          *snowflake.Snowflake
@@ -44,20 +52,20 @@ func (st *Strategy) GetStatistics() Statistics {
 func (st *Strategy) PrintStatistics() {
 	// Debug.
 	fmt.Printf("EXHAUSTED QUEUE: %d\n", len(st.exhaustedQueue))
-	for i, value := range st.exhaustedQueue {
-		fmt.Printf("queue: %d\t%d\n", i, value)
-	}
+	// for i, value := range st.exhaustedQueue {
+	// 	fmt.Printf("queue: %d\t%d\n", i, value)
+	// }
 
 	// Debug.
 	fmt.Printf("RESERVE QUEUE: %d\n", len(st.reserveQueue))
-	for i, value := range st.reserveQueue {
-		fmt.Printf("queue: %d\t%d\n", i, value)
-	}
+	// for i, value := range st.reserveQueue {
+	// 	fmt.Printf("queue: %d\t%d\n", i, value)
+	// }
 
 	// Debug.
-	fmt.Printf("\nID COUNT: %d\n", st.idCount)
+	fmt.Printf("ID COUNT: %d\n", st.idCount)
 	fmt.Printf("THREAD ID: %d\n", st.threadID)
-	fmt.Printf("EXHAUSTION MAX: %d\n", st.exhaustedMax)
+	fmt.Printf("EXHAUSTION MAX: %d\n\n", st.exhaustedMax)
 }
 
 func enqueue(queue []uint32, element uint32) []uint32 {
@@ -76,20 +84,54 @@ func dequeue(queue []uint32) ([]uint32, uint32) {
 	return queue[1:], element
 }
 
-func NewStrategy(Epoch time.Time, poolMinimum int, poolMaximum int) (*Strategy, error) {
+func NewStrategyPool(Epoch time.Time, EntryCount uint32, PoolStart uint32, ReservedPerPool uint32) (StrategyPool, error) {
+	// Make a new pool and set the index and counter.
+	sp := StrategyPool{}
+	sp.poolIndex = EntryCount - 1
+	sp.length = EntryCount
+
+	for i := PoolStart; i < EntryCount; i++ {
+		// Calculate the pool range.
+		start := PoolStart + i*ReservedPerPool
+		end := PoolStart + (i+1)*ReservedPerPool - 1
+
+		// Add a strategy for each entry.
+		st, err := NewStrategy(Epoch, start, end)
+		if err != nil {
+			fmt.Println("Failed to create a strategy pool.")
+			return sp, err
+		}
+		sp.Pool = append(sp.Pool, st)
+	}
+
+	return sp, nil
+}
+
+func (sp *StrategyPool) Next() *Strategy {
+	// Round robin.
+	sp.poolIndex++
+	if sp.poolIndex >= sp.length {
+		sp.poolIndex = 0
+	}
+
+	return sp.Pool[sp.poolIndex]
+}
+
+func NewStrategy(Epoch time.Time, PoolMinimum uint32, PoolMaximum uint32) (*Strategy, error) {
+	entries := PoolMaximum - PoolMinimum + 1
+
 	st := new(Strategy{
 		threadID:           0,
 		wasExhausted:       false,
 		exhaustedTimestamp: 0,
-		reserveQueue:       make(reservePoolType, 0, 64),
-		exhaustedQueue:     make(reservePoolType, 0, 64),
+		reserveQueue:       make(reservePoolType, 0, entries),
+		exhaustedQueue:     make(reservePoolType, 0, entries),
 		exhaustedMax:       0,
 		idCount:            0})
 
 	// Create a pool of IDS to hold in reserve.
-	for i := poolMinimum; i <= poolMaximum; i++ {
-		st.reserveQueue = append(st.reserveQueue, st.threadID)
-		st.threadID++
+	for i := PoolMinimum; i <= PoolMaximum; i++ {
+		st.reserveQueue = append(st.reserveQueue, PoolMinimum+i)
 	}
 
 	// Get an initial threadID.
@@ -103,8 +145,13 @@ func NewStrategy(Epoch time.Time, poolMinimum int, poolMaximum int) (*Strategy, 
 }
 
 func (st *Strategy) NextID() uint64 {
-	// DEBUG: waste some time so we can test the timestamp bucketing.
-	//time.Sleep(1000)
+	// Placing a mutex on this entire function will allow multiple
+	// clients to share the same strategy. You can comment this out
+	// if you're sure each execution thread / goroutine will have
+	// it's own strategy instance.
+	mutex := sync.Mutex{}
+	mutex.Lock()
+	defer mutex.Unlock()
 
 	// Get the next ID available.
 	id, isExhausted := st.snowflake.NextID()
--- tests/main.go
+++ tests/main.go
@@ -14,19 +14,32 @@ func main() {
 	// By inserting the values into a map we can easily check for duplicates.
 	m := make(threadIDMapType)
 
+	// Set the epoch to zero, the built-in default value will be used.
 	epoch := time.Time{}
-	st, err := strategy.NewStrategy(epoch, 0, 63)
+
+	// Create a pool of strategies to work with.
+	sp, err := strategy.NewStrategyPool(epoch, 4, 0, 16)
 	if err != nil {
-		fmt.Println("Failed to create a snowflake strategy.")
+		log.Fatal("Failed to create a snowflake strategy.")
 	}
 
-	fmt.Printf("Epoch: %v\n", epoch)
+	// st, err := strategy.NewStrategy(epoch, 0, 63)
+	// if err != nil {
+	// 	fmt.Println("Failed to create a snowflake strategy.")
+	// }
+
+	fmt.Printf("Epoch: %v\n\n", epoch)
 
 	for i := 0; i < 8192; i++ {
 		// DEBUG: waste some time so we can test the timestamp bucketing.
 		time.Sleep(time.Microsecond)
 
+		// Round robin the strategies.
+		st := sp.Next()
+
+		// Get the next snowflake.
 		id := st.NextID()
+
 		// Store it in a map for debugging purposes.
 		_, exists := m[id]
 		if !exists {
@@ -37,5 +50,7 @@ func main() {
 	}
 
 	// Print out the statistics for the run.
-	st.PrintStatistics()
+	for _, st := range sp.Pool {
+		st.PrintStatistics()
+	}
 }
